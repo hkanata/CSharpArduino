cedf57a70f3db1b11499eea47afff55208ae20dd
Miyamoto <hkanata@gmail.com>
2022-02-18 22:01:01 -0300

    Projeto
    Projeto

---
+++ ArduinoCSharp/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace ArduinoCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox_textReceiver = new System.Windows.Forms.RichTextBox();
+            this.textBox_textSent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label_status = new System.Windows.Forms.Label();
+            this.button_send = new System.Windows.Forms.Button();
+            this.button_open = new System.Windows.Forms.Button();
+            this.button_close = new System.Windows.Forms.Button();
+            this.comboBox_comPort = new System.Windows.Forms.ComboBox();
+            this.comboBox_baudRate = new System.Windows.Forms.ComboBox();
+            this.progressBar_statusBar = new System.Windows.Forms.ProgressBar();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.progressBar_statusBar);
+            this.groupBox1.Controls.Add(this.comboBox_baudRate);
+            this.groupBox1.Controls.Add(this.comboBox_comPort);
+            this.groupBox1.Controls.Add(this.button_close);
+            this.groupBox1.Controls.Add(this.button_open);
+            this.groupBox1.Controls.Add(this.label_status);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(10, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(227, 249);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "OPÇÕES";
+            // 
+            // richTextBox_textReceiver
+            // 
+            this.richTextBox_textReceiver.Location = new System.Drawing.Point(243, 61);
+            this.richTextBox_textReceiver.Name = "richTextBox_textReceiver";
+            this.richTextBox_textReceiver.Size = new System.Drawing.Size(532, 188);
+            this.richTextBox_textReceiver.TabIndex = 1;
+            this.richTextBox_textReceiver.Text = "";
+            this.richTextBox_textReceiver.TextChanged += new System.EventHandler(this.richTextBox_textReceiver_TextChanged);
+            // 
+            // textBox_textSent
+            // 
+            this.textBox_textSent.Location = new System.Drawing.Point(243, 27);
+            this.textBox_textSent.Name = "textBox_textSent";
+            this.textBox_textSent.Size = new System.Drawing.Size(451, 20);
+            this.textBox_textSent.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM PORT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BAUD RATE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "STATUS";
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_status.Location = new System.Drawing.Point(74, 123);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(135, 18);
+            this.label_status.TabIndex = 6;
+            this.label_status.Text = "DESCONECTADO";
+            // 
+            // button_send
+            // 
+            this.button_send.Location = new System.Drawing.Point(700, 25);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(75, 23);
+            this.button_send.TabIndex = 3;
+            this.button_send.Text = "Enviar";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.button_send_Click);
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(5, 177);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(102, 62);
+            this.button_open.TabIndex = 4;
+            this.button_open.Text = "Abrir";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // button_close
+            // 
+            this.button_close.Location = new System.Drawing.Point(119, 177);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(102, 62);
+            this.button_close.TabIndex = 7;
+            this.button_close.Text = "Fechar";
+            this.button_close.UseVisualStyleBackColor = true;
+            this.button_close.Click += new System.EventHandler(this.button_close_Click);
+            // 
+            // comboBox_comPort
+            // 
+            this.comboBox_comPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_comPort.FormattingEnabled = true;
+            this.comboBox_comPort.Location = new System.Drawing.Point(77, 15);
+            this.comboBox_comPort.Name = "comboBox_comPort";
+            this.comboBox_comPort.Size = new System.Drawing.Size(144, 21);
+            this.comboBox_comPort.TabIndex = 8;
+            // 
+            // comboBox_baudRate
+            // 
+            this.comboBox_baudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_baudRate.FormattingEnabled = true;
+            this.comboBox_baudRate.Items.AddRange(new object[] {
+            "9600",
+            "38400",
+            "115200"});
+            this.comboBox_baudRate.Location = new System.Drawing.Point(77, 51);
+            this.comboBox_baudRate.Name = "comboBox_baudRate";
+            this.comboBox_baudRate.Size = new System.Drawing.Size(144, 21);
+            this.comboBox_baudRate.TabIndex = 9;
+            // 
+            // progressBar_statusBar
+            // 
+            this.progressBar_statusBar.Location = new System.Drawing.Point(77, 88);
+            this.progressBar_statusBar.Name = "progressBar_statusBar";
+            this.progressBar_statusBar.Size = new System.Drawing.Size(144, 21);
+            this.progressBar_statusBar.TabIndex = 10;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 269);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.textBox_textSent);
+            this.Controls.Add(this.richTextBox_textReceiver);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CSharp e Arduino";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox_textReceiver;
+        private System.Windows.Forms.TextBox textBox_textSent;
+        private System.Windows.Forms.Button button_send;
+        private System.Windows.Forms.Button button_close;
+        private System.Windows.Forms.ProgressBar progressBar_statusBar;
+        private System.Windows.Forms.ComboBox comboBox_baudRate;
+        private System.Windows.Forms.ComboBox comboBox_comPort;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
